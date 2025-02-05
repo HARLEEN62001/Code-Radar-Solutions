3cc283d777b47b87818e5df64bd40796eab102bf
HARLEEN62001 <kaurharleen95927@gmail.com>
2025-02-05 20:53:42 +0530

    Solution to Input and Print a Float

--- Input and Print a Float@@6788d5012db25efec946f93a/code.c
+++ Input and Print a Float@@6788d5012db25efec946f93a/code.c
@@ -3,16 +3,14 @@
 int main() {
     float num;
     
-    // Read the input float directly without the prompt
-    if (scanf("%f", &num) == 1) {
-        // Print the float with 2 decimal places
+
+    if (scanf("%f", &num) == 1); {
+        
         printf("%.2f\n", num);
     } else {
-        // If the input is not valid, display an error message
+        
         printf("Invalid input\n");
     }
 
     return 0;
 }
-
-
