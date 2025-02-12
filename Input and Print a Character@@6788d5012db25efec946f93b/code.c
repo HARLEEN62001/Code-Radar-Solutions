6aa081360471901c34a6da92749b3a8a6bec9b0e
HARLEEN62001 <kaurharleen95927@gmail.com>
2025-02-12 15:07:07 +0530

    Solution to Input and Print a Character

--- Input and Print a Character@@6788d5012db25efec946f93b/code.c
+++ Input and Print a Character@@6788d5012db25efec946f93b/code.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-
-int main() {
-    float num;
-    
-    // Read the input float directly without the prompt
-    scanf("%f", &num);
-
-    // Print the float with 2 decimal places
-    printf("%.2f\n", num);
-
+int main(){
+char a;
+scanf("%c",&a);
+printf("You entered: %c",a);
     return 0;
 }
